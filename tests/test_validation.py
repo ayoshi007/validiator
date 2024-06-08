9395cd0bbe8cca738b2664fd3d318ad538ec6d94
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-06-08 17:37:25 -0500

    Add tests for dictionary validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -33,6 +33,12 @@ class SampleGroup:
         pytest.param("hi", bool, True, id="bad_bool"),
         pytest.param([1], int, True, id="sequence_vs_primitive"),
         pytest.param({"a": 1}, str, True, id="mapping_vs_primitive"),
+        pytest.param(
+            SamplePerson("name", 1, False, 10.5, [], {}, None),
+            str,
+            True,
+            id="dataclass_vs_primitive",
+        ),
     ],
 )
 def test_validate_primitives(data, datatype, should_fail: bool):
@@ -48,13 +54,60 @@ def test_validate_primitives(data, datatype, should_fail: bool):
     [
         pytest.param(["hi"], List[str], False, id="good_list_of_strs"),
         pytest.param([1], List[int], False, id="good_list_of_ints"),
-        pytest.param([[1], [2]], List[List[int]], False,
-                     id="good_list_of_list_of_ints"),
-        pytest.param(1.1, List[float], True, id="primitive_vs_sequence"),
-        pytest.param({"str": 1}, List[str], True, id="mapping_vs_sequence"),
+        pytest.param(
+            [[1], [2]], List[List[int]], False, id="good_list_of_list_of_ints"
+        ),
+        pytest.param(
+            [[1], [2], 1], List[List[int]], True, id="bad_list_of_list_of_ints"
+        ),
+        pytest.param(1.1, List[float], True, id="primitive_vs_list"),
+        pytest.param({"str": 1}, List[str], True, id="mapping_vs_list"),
+        pytest.param(
+            SamplePerson("name", 1, False, 10.5, [], {}, None),
+            List[str],
+            True,
+            id="dataclass_vs_list",
+        ),
     ],
 )
 def test_validate_lists(data, datatype, should_fail: bool):
+    mesg: str | None = validate(data, datatype)
+    if should_fail:
+        assert mesg is not None, mesg
+    else:
+        assert mesg is None, mesg
+
+
+@pytest.mark.parametrize(
+    ["data", "datatype", "should_fail"],
+    [
+        pytest.param({"a": 1}, Dict[str, int], False, id="good_dict_str_int"),
+        pytest.param(
+            {True: 1.1}, Dict[bool, float], False, id="good_dict_of_bool_float"
+        ),
+        pytest.param(
+            {"a": {"b": 1}},
+            Dict[str, Dict[str, int]],
+            False,
+            id="good_dict_of_str_dict_of_str_int",
+        ),
+        pytest.param(
+            {"a": {"b": 1}, True: {"b": 1}},
+            Dict[str, Dict[str, int]],
+            True,
+            id="bad_dict_of_str_dict_of_str_int",
+        ),
+        pytest.param(1.1, Dict[str, float], True, id="primitive_vs_dict"),
+        pytest.param(["str"], Dict[str, str], True, id="list_vs_dict"),
+        pytest.param(
+            SamplePerson("name", 1, False, 10.5, [], {}, None),
+            Dict[str, str],
+            True,
+            id="dataclass_vs_dict",
+        ),
+    ],
+)
+def test_validate_dictionaries(data, datatype, should_fail: bool):
     mesg: str | None = validate(data, datatype)
     if should_fail:
         assert mesg is not None, mesg
